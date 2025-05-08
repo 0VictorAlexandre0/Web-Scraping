da24c99aad537d04be1d36c8a3f809ac36242d2b
Gabriela Araujo Rodrigues <155750197+0VictorAlexandre0@users.noreply.github.com>
2025-05-08 17:34:36 -0300

    desistencia

--- projeto.py
+++ projeto.py
@@ -5,29 +5,28 @@ import json
 url = 'https://infosimples.com/vagas/desafio/commercia/product.html'
 
 resposta = {}
-
 response = requests.get(url)
-
 parsed_html = BeautifulSoup(response.content, 'html.parser')
-resposta['title'] = parsed_html.select_one('h2#product_title').get_text()
-resposta['brand'] = parsed_html.select_one('div.brand').get_text()
 
-lista = []
-'''
-resposta['categories'] = parsed_html.select_one('nav.current-category').get_text()
-for item in resposta['categories']:
-    lista.append(item)
-'''
 
-'''
-for item in parsed_html.select_one('nav.current-category a'):
-   lista.append(item.get_text())
+resposta['title'] = parsed_html.select_one('h2#product_title').get_text(strip=True)
 
-print(lista)
-'''
+resposta['brand'] = parsed_html.select_one('div.brand').get_text(strip=True)
 
-resposta['categories'] = parsed_html.select_one('nav.current-category').get_text()
-print(resposta['categories'(1)])
+resposta['categories'] = [a.get_text(strip=True) 
+    for a in parsed_html.select('nav.current-category a')]
+
+resposta['description'] = [p.get_text(strip=True) 
+    for p in parsed_html.select('div.proddet p')]
+
+skus = []
+for bloco in parsed_html.select('div.sku'):
+    nome = bloco.select_one('span.sku-name').get_text(strip=True)
+    skus.append(nome)
+
+resposta['skus'] = skus
+
+
 
 
 
@@ -35,6 +34,7 @@ print(resposta['categories'(1)])
 
 json_resposta_final = json.dumps(resposta)
 
+
 with open('teste.json', 'w') as arquivo_json:
- arquivo_json.write(json_resposta_final)
+    json.dump(resposta, arquivo_json, ensure_ascii=False, indent=2)
 
